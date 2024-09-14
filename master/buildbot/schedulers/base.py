8ba352e3bb1272495c42fa2bc4ebb7bbf136dbb8
Dmitry Vasilyev <dvassiliev@accesssoftek.com>
2024-09-14 16:36:31 +0400

    Support the parameter ignoreOfflineWorkersTimeout in master.cfg
    ignoreOfflineWorkersTimeout=60 means do not create build requests for builders which have no active workers more than 60 minutes.
    ignoreOfflineWorkersTimeout=0 disables this feature.

--- master/buildbot/schedulers/base.py
+++ master/buildbot/schedulers/base.py
@@ -445,7 +445,13 @@ class BaseScheduler(ClusteredBuildbotService, StateMixin):
         builderids = []
         for bldr in (yield self.master.data.get(('builders', ))):
             if bldr['name'] in builderNames:
-                builderids.append(bldr['builderid'])
+                #LLVM_LOCAL
+                bldr_obj = self.master.botmaster.builders.get(bldr['name'])
+                # Check the attribute to make tests with mock Builder happy.
+                if not hasattr(bldr_obj.__class__, "workersAvailable") or bldr_obj.workersAvailable():
+                    builderids.append(bldr['builderid'])
+                else:
+                    log.msg(f">>> addBuildsetForSourceStamps: Ignore builder {bldr['name']} because of offline workers.")
 
         # translate properties object into a dict as required by the
         # addBuildset method
